d1321c48433a59655f5054594de3a218866143ea
Antti Pyykkönen <antti.pyykkonen@gmail.com>
2020-02-01 23:13:50 +0200

    Add piranha plant prefab

---
+++ Assets/Scripts/DamageBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DamageBehaviour : MonoBehaviour
+{
+    private LevelManager _levelManager;
+    
+    public void Awake()
+    {
+        _levelManager = FindObjectOfType<LevelManager>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerMovement playerMovement = other.gameObject.GetComponent<PlayerMovement>();
+        if(playerMovement != null)
+        {
+            if (LevelManager.isMushroomMode)
+            {
+                _levelManager.InterruptMushroomMode();
+            }
+        }
+
+    }
+}
