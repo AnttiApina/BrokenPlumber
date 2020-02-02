167b2a6c1efb2887eaf0bef49bc4d1d3da0d426e
Antti Pyykkönen <antti.pyykkonen@gmail.com>
2020-02-02 07:05:03 +0200

    Improve base game scene & add intro exit trigger

---
+++ Assets/IntroContro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+public class IntroContro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GoToStart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            GoToStart();
+        }
+    }
+}
